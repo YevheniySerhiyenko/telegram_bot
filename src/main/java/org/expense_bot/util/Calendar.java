89ddf770cea55c006d4bcb02aec4f812e9b03e6a
YevheniySerhiyenko <johnbellostory2@gmail.com>
2022-11-01 12:30:36 +0200

    Telegram bot

--- src/main/java/org/expense_bot/util/Calendar.java
+++ src/main/java/org/expense_bot/util/Calendar.java
@@ -1,11 +1,6 @@
 package org.expense_bot.util;
 
-import org.python.util.PythonInterpreter;
 public class Calendar {
-	public static void main(String[] args) {
-	  try (PythonInterpreter pyInterp = new PythonInterpreter()) {
-		pyInterp.exec("print('Hello Python World!')");
-	  }
-	}
+
 
 }
